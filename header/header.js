f449809b3e2404a8a2aaf6b5ef89942c37bf8faa
Itaru7 <ir172178@gmail.com>
2017-06-02 16:27:35 -0700

    header and myPage

---
+++ header/header.js
@@ -0,0 +1,34 @@
+// Header
+
+var result = "";
+	result += "<link rel=\"stylesheet\" type=\"text/css\" href=\"header.css\" />"
+			+"<span class = \"logo\">"
+		   		+ "<img src=\"\" />"
+        	 	+ "logo"
+    		+ "</span>"
+			
+    		+ "<span class = \"search\">"
+    			+ "<input placeholder=\"Search\" />"
+    		+ "</span>"
+			
+			+"<span class = \"home\">"
+    			+ "Home"
+			+ "</span>"
+			
+			+"<span class = \"home\">"
+    		+ "Jobs"
+			+ "</span>"
+			
+			+"<span class = \"message\">"
+    		+ "Messaging"
+			+ "</span>"
+			
+			+"<span class = \"notification\">"
+    		+ "Notifications"
+			+ "</span>"
+			
+			+"<span class = \"myPage\">"
+    		+ "My page"
+			+ "</span>";
+	
+document.write(result);
